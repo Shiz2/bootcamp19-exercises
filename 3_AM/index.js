1d9ef89586588843dc13a5ec263a16ccc94c2897
Sela <sela.kasepa@gmail.com>
2019-01-09 12:51:44 -0500

    3_AM website project

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static('public'))
+
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + '/View/index.html'));
 });
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname + '/View/about.html'));
+})
+
+app.get("/foods", (req,res) => {
+  res.sendFile(path.join(__dirname + '/View/food.html'));
+})
+
+app.get("/sites", (req, res) => {
+  res.sendFile(path.join(__dirname + '/View/sites.html'));
+})
 
 app.listen(3000);
 console.log("Running at port 3000");
